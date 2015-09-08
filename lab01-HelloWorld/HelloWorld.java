1ab66b1723056defa900e13b643b6563b5fb5d28
clarisakelly <oxrissixo@gmail.com>
2015-09-08 18:42:08 +0000

    Hellow World

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,22 @@
+// Clarisa Kelly
+// Lab 01
+// Hello World Java Program
+
+//define a class
+public class HelloWorld{
+    
+    //add main method
+    public static void main(String[] args){
+        
+//print statement with box around it
+System.out.println(" ");
+System.out.println("########################");
+System.out.println("########################");
+System.out.println("#                      #");
+System.out.println("#  Hello World 2015!!  #");
+System.out.println("#                      #");
+System.out.println("########################");
+System.out.println("########################");
+System.out.println("testing");
+    }
+}
